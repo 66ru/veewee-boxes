95f9e6c75784ca5e90d2ea22c4b845bef11a1872
Victor Sokolov <vsokolov987234@gmail.com>
2013-06-10 15:31:54 +0600

    mkdir /var/chef-solo
    add /var/chef-solo, so that chef-solo's first run would not break

--- definitions/squeeze/chef.sh
+++ definitions/squeeze/chef.sh
@@ -6,3 +6,5 @@
 # А так ставим его мы, ибо мы используем слегка устаревшую версию:
 wget https://opscode-omnibus-packages.s3.amazonaws.com/debian/6/x86_64/chef_10.24.0-1.debian.6.0.5_amd64.deb
 dpkg -i chef_10.24.0-1.debian.6.0.5_amd64.deb
+
+mkdir -p /var/chef-solo
